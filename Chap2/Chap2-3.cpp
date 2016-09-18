18da426e347d6d94186f41e6ec54b86cbc640879
Genesis2011 <2576340060@qq.com>
2016-09-18 15:17:28 +0800

    2-3

---
+++ Chap2/Chap2-3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+ * 先计当前数的半数集个数为1，再判断当前数是否大于1
+ * 如果大于1，递归计算1~n/2间个数；否则，直接返回个数
+ */
+int set(int n) {
+    int count = 1;
+    if(n > 1)
+        for(int i=1; i<=n/2; i++)
+            count += set(i);
+    return count;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<set(n)<<endl;
+    return 0;
+}
