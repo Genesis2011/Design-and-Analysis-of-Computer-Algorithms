c3c37a8bf8a4bdc7e9b3604b8c06699fd5353fcf
Genesis2011 <2576340060@qq.com>
2016-10-16 10:49:00 +0800

    3-4 note

--- Chap3/Chap3-4/Chap3-4.cpp
+++ Chap3/Chap3-4/Chap3-4.cpp
@@ -7,6 +7,18 @@ using namespace std;
 int D[MAX][MAX];
 int maxSum[MAX][MAX];
 
+/*
+ * D(r,j)：第r行第j个数字（r，j从1开始算）
+ * MaxSum(r,j)：从D(r,j)到底边的各条路径中，最佳路径的数字之和
+ *
+ * D(r,j)出发，下一步只能走D(r+1,j)或者D(r+1,j+1)
+ * 对于N行的三角形：
+ * if(r == N)
+ *     MaxSum(r,j) = D(r,j)
+ * else
+ *     MaxSum(r,j) = Max{ MaxSum(r+1,j), MaxSum(r+1,j+1) } + D(r,j)
+ */
+
 int main(){
     int n;
     ifstream in("input.txt");
